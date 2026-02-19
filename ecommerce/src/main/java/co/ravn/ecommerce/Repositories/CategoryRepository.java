97133ae0020b7df44b1ea712b01a954d895197bd
Israel Lopez <israellopez@ravn.co>
2026-02-19 12:11:27 -0500

    Remove duplicates files from merge

--- ecommerce/src/main/java/co/ravn/ecommerce/Repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package co.ravn.ecommerce.Repositories;
-
-import co.ravn.ecommerce.Entities.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository  extends JpaRepository<Category, Integer> {
-}
