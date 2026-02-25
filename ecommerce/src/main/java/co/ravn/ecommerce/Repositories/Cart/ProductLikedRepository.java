f2287fc3262339128982a97c7e74eac697d568a0
Israel Lopez <israellopez@ravn.co>
2026-02-25 17:10:34 -0500

    feat: added email for price drop (discount) for products liked

--- ecommerce/src/main/java/co/ravn/ecommerce/Repositories/Cart/ProductLikedRepository.java
+++ ecommerce/src/main/java/co/ravn/ecommerce/Repositories/Cart/ProductLikedRepository.java
@@ -2,6 +2,8 @@ package co.ravn.ecommerce.Repositories.Cart;
 
 import co.ravn.ecommerce.Entities.Cart.ProductLiked;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,12 @@ public interface ProductLikedRepository extends JpaRepository<ProductLiked, Inte
     List<ProductLiked> findByProductId(int productId);
     Optional<ProductLiked> findByProductIdAndUserId(int productId, int userId);
     List<ProductLiked> findByProductIdAndHasBeenNotifiedFalseOrHasBeenNotifiedIsNull(int productId);
+
+    @Query("SELECT pl FROM ProductLiked pl " +
+           "WHERE pl.product.id = :productId " +
+           "AND (pl.hasBeenNotified = false OR pl.hasBeenNotified IS NULL) " +
+           "AND pl.user.isActive = true " +
+           "AND pl.product.isActive = true " +
+           "AND pl.product.deletedAt IS NULL")
+    List<ProductLiked> findActiveUnnotifiedByProductId(@Param("productId") int productId);
 }
